f4672479915e55322ea3b1bd6b6f2b85915d1406
roberthsu <roberthsu2003@gmail.com>
2026-09-13 11:19:52 +0800

    modify

--- 0913/practice1.py
+++ 0913/practice1.py
@@ -11,8 +11,8 @@ TELEGRAM_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
 
 # 設定不同推播目標
 TARGET_USER_ID = 8719067870               # 個人 (正整數，用戶需先私訊過 Bot)
-TARGET_GROUP_ID = "-5339618596"         # 群組 (負整數，Bot 需在群組內)
-TARGET_CHANNEL_ID = "-1004322810613"     # 頻道 Chat ID (Bot 需為管理員)
+TARGET_GROUP_ID = -5339618596         # 群組 (負整數，Bot 需在群組內)
+TARGET_CHANNEL_ID = -1004322810613     # 頻道 Chat ID (Bot 需為管理員)
 
 async def send_broadcast(chat_id: str | int, message: str):
     bot = Bot(token=TELEGRAM_TOKEN)
